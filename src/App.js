fd5668967e9261b06f69491d1e73e2be1d95f348
hyunjeong <cchloe0927@gmail.com>
2023-01-03 10:11:59 +0900

    feat : 전역 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
+import GlobalStyles from "./shared/GlobalStyles";
 import Router from "./shared/Router";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
