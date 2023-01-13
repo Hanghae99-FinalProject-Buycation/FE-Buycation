d4bd63e523be76cfd9eacd6830dc7d7686c038c7
hyunjeong <cchloe0927@gmail.com>
2023-01-13 15:26:17 +0900

    feat : 헤더 아이콘 추가 및 라우터 연동

--- src/shared/layout/Header.jsx
+++ src/shared/layout/Header.jsx
@@ -1,11 +1,29 @@
 import styled from "@emotion/styled";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import posting from "../../assets/posting.svg";
+import chatting from "../../assets/chatting.svg";
+import alarm from "../../assets/alarm.svg";
+import myprofile from "../../assets/myprofile.svg";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const onMovePostingHandler = () => {
+    navigate("/posting");
+  };
+  const onMoveProfileHandler = () => {
+    navigate("/profile");
+  };
+
   return (
     <HeaderDiv>
       <Link to={"/"}>바이케이션</Link>
+      <Icon>
+        <img src={posting} onClick={onMovePostingHandler} />
+        <img src={chatting} />
+        <img src={alarm} />
+        <img src={myprofile} onClick={onMoveProfileHandler} />
+      </Icon>
     </HeaderDiv>
   );
 };
@@ -13,13 +31,17 @@ const Header = () => {
 export default Header;
 
 const HeaderDiv = styled.div`
-  border-bottom: 1px solid ${({ theme }) => theme.colors.main};
+  height: 60px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 60px;
-  padding: 0.5rem;
+  padding: 2rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.main};
   & > a {
     color: black;
   }
+`;
+const Icon = styled.div`
+  display: flex;
+  gap: 23px;
 `;
