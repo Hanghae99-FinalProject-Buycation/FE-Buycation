4d01a73108b07a4fca037100b7f3836d42c7c3ee
Dev-RyanK <dev.ryan.kwon@gmail.com>
2023-01-19 13:25:00 +0900

    feat: 헤더 로고 추가 및 아이콘 분기 설정

--- src/shared/layout/Header.jsx
+++ src/shared/layout/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import SmallModal from "../../components/headerModal/userModal";
+import ButtonBasic from "../../components/elements/ButtonBasic";
 import postingIcon from "../../assets/headerIcon/postingIcon.svg";
 import chattingIcon from "../../assets/headerIcon/chattingIcon.svg";
 import alarmIcon from "../../assets/headerIcon/alarmIcon.svg";
 import profileIcon from "../../assets/headerIcon/profileIcon.svg";
+import logo from "../../assets/headerIcon/buycationLogo.svg";
+import logoHover from "../../assets/headerIcon/buycationLogoHover.svg";
 import { sendModalStatus } from "../../redux/modules/modal/modalSlice";
-import { removeCookies } from "../../core/cookie";
+import { getCookies, removeCookies } from "../../core/cookie";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import useWindowResize from "../../hooks/useWindowResize";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const token = getCookies("id");
   const modalStatus = useSelector((state) => state.generalModal.toggleModal);
-
+  const { innerWidth } = useWindowResize();
   const onMovePostingHandler = () => {
     navigate("/posting");
   };
@@ -41,17 +47,37 @@ const Header = () => {
 
   return (
     <HeaderDiv>
-      <Link to={"/"}>바이케이션</Link>
-      <Icon>
-        <img alt="posting" src={postingIcon} onClick={onMovePostingHandler} />
-        <img alt="chatting" src={chattingIcon} />
-        <img alt="alarm" src={alarmIcon} />
-        <img
-          alt="profile"
-          src={profileIcon}
-          onClick={onClickMypageModalHandler}
-        />
-      </Icon>
+      <Logo alt="바이케이션" onClick={() => navigate("/")} />
+      {token ? (
+        innerWidth > 450 ? (
+          <Icon>
+            <img
+              alt="posting"
+              src={postingIcon}
+              onClick={onMovePostingHandler}
+            />
+            <img alt="chatting" src={chattingIcon} />
+            <img alt="alarm" src={alarmIcon} />
+            <img
+              alt="profile"
+              src={profileIcon}
+              onClick={onClickMypageModalHandler}
+            />
+          </Icon>
+        ) : (
+          <HiOutlineBars3 size="1.5rem" onClick={onClickMypageModalHandler} />
+        )
+      ) : (
+        <ButtonBasic
+          width="4rem"
+          height="2rem"
+          borderRadius="2rem"
+          _onClick={onMoveLoginHandler}
+        >
+          로그인
+        </ButtonBasic>
+      )}
+
       {!modalStatus && (
         <SmallModal
           top="4rem"
@@ -62,7 +88,6 @@ const Header = () => {
           secondClick={onMoveLoginHandler}
           third="로그아웃"
           thirdClick={onMoveLogoutHandler}
-          // hidden={!modalStatus}
         />
       )}
     </HeaderDiv>
@@ -82,6 +107,17 @@ const HeaderDiv = styled.div`
     color: black;
   }
 `;
+
+const Logo = styled.div`
+  height: 2.8rem;
+  width: 12.5rem;
+  margin-bottom: 1rem;
+  background: url(${logo}) bottom/100% no-repeat;
+  :hover {
+    background: url(${logoHover}) bottom/100% no-repeat;
+  }
+`;
+
 const Icon = styled.div`
   display: flex;
   gap: 23px;
