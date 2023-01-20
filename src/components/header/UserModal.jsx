343c54ed79693b6b0b759388ddfd8c55bca06b7f
hyunjeong <cchloe0927@gmail.com>
2023-01-20 10:04:06 +0900

    feat : 헤더 관련 폴더 구조 재정리

--- src/components/header/UserModal.jsx
+++ src/components/header/UserModal.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 import { useDispatch, useSelector } from "react-redux";
 import useOutsideClick from "../../hooks/useOutsideClick";
 import { sendModalStatus } from "../../redux/modules/modal/modalSlice";
 import { getCookies } from "../../core/cookie";
 
-const SmallModal = (props) => {
+const UserModal = (props) => {
   const {
     first,
     second,
@@ -53,7 +53,7 @@ const SmallModal = (props) => {
   );
 };
 
-export default SmallModal;
+export default UserModal;
 
 const StPostingOption = styled.div`
   width: 90px;
