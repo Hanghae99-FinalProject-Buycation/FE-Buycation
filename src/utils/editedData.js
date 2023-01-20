6996f8bdd751d48e972146f398b361ec85fecede
Dev-RyanK <dev.ryan.kwon@gmail.com>
2023-01-20 16:33:13 +0900

    fix: countComme 옵셔널 체이닝 추가

--- src/utils/editedData.js
+++ src/utils/editedData.js
@@ -21,11 +21,11 @@ export const longTitleForm = (title) => {
   if (title?.length < 60) {
     return title;
   } else {
-    return title.substr(0, 60) + "...";
+    return title?.substr(0, 60) + "...";
   }
 };
 
 // 금액 자릿수 추가
 export const countComma = (money) => {
-  return money.toLocaleString("ko-KR") + "원";
+  return money?.toLocaleString("ko-KR") + "원";
 };
