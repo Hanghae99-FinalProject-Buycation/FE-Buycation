925e5730783f61eb25abbdefedac7031ae371868
hyunjeong <cchloe0927@gmail.com>
2023-01-27 21:14:08 +0900

    feat : SSE 알림 기능 구현

--- src/shared/layout/Header.jsx
+++ src/shared/layout/Header.jsx
@@ -31,70 +31,42 @@ const Header = () => {
   const [alarmCount, setAlarmCount] = useState();
 
   //알람 구독
-  // useEffect(() => {
-  //   if (tokenValue) {
-  //     let eventSource = new EventSource(
-  //       `${BACK_API}/alarm/subscribe/1`, //임시
-  //       {
-  //         headers: {
-  //           Authorization: tokenValue,
-  //           // lastEventId: "1_10101010",
-  //         },
-  //       }
-  //     );
-  //     //EVENTSOURCE ONMESSAGE
-  //     eventSource.onmessage = (event) => {
-  //       console.log(event.data);
-  //       dispatch(__getAlarmCount());
-  //     };
-  //     //EVENTSOURCE ONERROR
-  //     eventSource.onerror = (event) => {
-  //       console.log(event.error.message);
-  //       console.log(!event.error.message.includes("No activity"));
-  //       if (!event.error.message.includes("No activity")) {
-  //         console.log("이벤트 종료");
-  //         eventSource.close();
-  //       }
-  //     };
-  //   }
-  // }, [tokenValue, dispatch, EventSource]);
+  useEffect(() => {
+    if (tokenValue) {
+      let eventSource;
+      const fetchSSE = async () => {
+        try {
+          eventSource = new EventSource(`${BACK_API}/alarm/subscribe`, {
+            headers: {
+              Authorization: tokenValue,
+            },
+            heartbeatTimeout: 60 * 1000 * 30, //30분
+            //withCredentials: true, //크로스 도메인에 요청을 보낼 때 요청에 credential 정보를 담아서 보낼 지를 결정하는 항목
+          });
 
-  // useEffect(() => {
-  //   if (tokenValue) {
-  //     let eventSource;
-  //     const fetchSSE = async () => {
-  //       try {
-  //         eventSource = new EventSource(`${BACK_API}/alarm/subscribe`, {
-  //           headers: {
-  //             Authorization: tokenValue,
-  //           },
-  //           //heartbeatTimeout: 60 * 1000 * 5, //5분
-  //           withCredentials: true, //크로스 도메인에 요청을 보낼 때 요청에 credential 정보를 담아서 보낼 지를 결정하는 항목
-  //         });
+          /* EVENTSOURCE ONMESSAGE : 서버로부터 message를 수신했을 때 호출하는 이벤트 핸들러 */
+          eventSource.onmessage = async (event) => {
+            const res = await event.data;
+            console.log(res);
+            setAlarmCount(res);
+            //dispatch(__getAlarmCount());
+          };
 
-  //         /* EVENTSOURCE ONMESSAGE : 서버로부터 message를 수신했을 때 호출하는 이벤트 핸들러 */
-  //         eventSource.onmessage = async (event) => {
-  //           const res = await event.data;
-  //           console.log(res);
-  //           setAlarmCount(res);
-  //           //dispatch(__getAlarmCount());
-  //         };
-
-  //         /* EVENTSOURCE ONERROR : 에러가 발생하거나 EventSource 객체에서 error event가 감지되었을 때 호출하는 이벤트 핸들러 */
-  //         eventSource.onerror = async (event) => {
-  //           console.log("에러 이벤트 발생");
-  //           console.log(event.error.message);
-  //           if (!event.error.message.includes("No activity")) {
-  //             console.log("에러 이벤트 발생 왜 안하냐?");
-  //             eventSource.close();
-  //           }
-  //         };
-  //       } catch (error) {}
-  //     };
-  //     fetchSSE();
-  //     //return () => eventSource.close();
-  //   }
-  // }, [tokenValue, dispatch, EventSource, alarmCount]);
+          /* EVENTSOURCE ONERROR : 에러가 발생하거나 EventSource 객체에서 error event가 감지되었을 때 호출하는 이벤트 핸들러 */
+          eventSource.onerror = async (event) => {
+            console.log("45000 에러 이벤트 발생");
+            console.log(event.error.message);
+            if (!event.error.message.includes("No activity")) {
+              console.log("에러 이벤트 발생");
+              eventSource.close();
+            }
+          };
+        } catch (error) {}
+      };
+      fetchSSE();
+      return () => eventSource.close();
+    }
+  }, [tokenValue, EventSource]);
 
   //alarm 조회, 삭제, 수정
   const onClickAlarmModalHandler = () => {
